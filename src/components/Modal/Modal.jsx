fad225b784ccb558224990b2c58b0aa5ed6ee21f
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-05-24 13:24:28 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react'
 import s from './Modal.module.css'
 
 const Modal = ({children, title = 'Default modal', onClose }) => {
 // до цього handleBackDropClick все робило 
+    const handleBackDropClick = e => {
+        console.log('TARGET :>>>>', e.target)
+        console.log('CURRENT TARGET :>>>>', e.currentTarget)
+        if(e.target === e.currentTarget){
+            onClose()
+        }
+        
+    }
+ // до цього  все робило 
 
- 
+useEffect(() => {
+    const  hadleKeyDown = e => {
+        console.log(e.key);
+        if(e.key === 'Escape') {
+            onClose()
+        }
+    }
+    document.addEventListener('keydown', hadleKeyDown)
+},[onClose])
+
+
     return (
-        <div className={s.wrapper}>
+        <div className={s.wrapper} onClick={handleBackDropClick}>
             <div className={s.content}>
                 <>
                     <h1>{title}</h1>
